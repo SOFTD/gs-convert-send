ee60e3155940b191b5d5a34338a1c94514889b6e
Luisma <lmarcelo.rodriguez@outlook.com>
2020-05-26 14:20:41 -0400

    Create test-gs.py

---
+++ test-gs.py
@@ -0,0 +1,5 @@
+from GS import GS_to_DF
+creds='test_sheets_client_secret.json'
+url='https://docs.google.com/spreadsheets/d/........../edit#gid=0'
+sheet='shhet2'
+reporte=GS_to_DF(url=url,sheet=sheet,credenciales=creds)
